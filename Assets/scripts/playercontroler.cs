b112aa2a81c49183741cb89b21ea43f542115ff2
gurus4269 <1017johnny@gmail.com>
2024-08-04 16:18:07 +0800

    2024/08/04 初次上傳

---
+++ Assets/scripts/playercontroler.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class playercontroler : MonoBehaviour
+{
+    public float movespeed = 5f;
+    private float InputX;
+
+    private bool isFlip = false;
+
+    private Rigidbody2D rig;
+    private Animator ani;
+
+    public Transform floor;
+
+    public LayerMask groundMask;
+
+    private bool isground;
+
+    public void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        rig.velocity = new Vector2(movespeed * InputX, rig.velocity.y);
+
+        //判斷移動
+        ani.SetBool("isrun", Mathf.Abs(rig.velocity.x) > 0);
+
+        //判斷跳躍
+        ani.SetBool("isgrounded", isground);
+        ani.SetFloat("yVelocity", rig.velocity.y); 
+
+        //判斷翻轉
+        if(!isFlip)
+        {
+            if(rig.velocity.x < 0)
+            {
+                isFlip = true;
+                transform.Rotate(0.0f, 180.0f, 0.0f);
+            }
+        }
+        else 
+        {
+            if(rig.velocity.x > 0)
+            {
+                isFlip = false;
+                transform.Rotate(0.0f, 180.0f, 0.0f);
+            }
+        }
+
+        //判斷觸地
+        isground = Physics2D.OverlapCircle(floor.position, .2f, groundMask);
+
+           
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        InputX = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(isground)
+        {
+            rig.velocity = new Vector2(rig.velocity.x,5);
+        }
+    }
+
+    private void OnDrawGizmos() 
+    {
+        Gizmos.DrawWireSphere(floor.position, .2f);
+    }
+
+}
